c628194e3f0c75fcbf552e834528caa6f49c6246
rruiqiu <qiur12@mcmaster.ca>
2024-07-15 00:15:22 -0400

    disable github cli instead of using vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import BG from '@/components/IntroBg'
+import BG from '@/components/IntroBgServer'
 const Home = () => {
   return (
     <main>
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basepath: "/pw-frontend",
-  output: "export",  // <=== enables static exports
+  // basepath: "/pw-frontend",
+  // output: "export",  // <=== enables static exports
   reactStrictMode: true,
   images: { unoptimized: true }
 };
