17a67653d8fac7c49f6f4d2e333e9297342842f8
rruiqiu <qiur12@mcmaster.ca>
2024-07-21 21:45:35 -0400

    s

--- app/components/IntroBgServer.tsx
+++ app/components/IntroBgServer.tsx
@@ -10,7 +10,7 @@ const Bg = async () => {
     const requestOptions: RequestInit = {
       method: 'GET',
       redirect: 'follow',
-      // cache: 'no-store',
+      cache: 'no-store',
     }
     const response = await fetch(
       'https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=en-CA',
@@ -21,7 +21,7 @@ const Bg = async () => {
       throw new Error('Failed')
     }
 
-    const data = await response.json()
+    const data = response.json()
     // console.log(data)
     return data
   }
