185dc9517e3ae98869f79d04d4c2462b4eebe364
rruiqiu <qiur12@mcmaster.ca>
2024-07-22 00:07:53 -0400

    switch another api point

--- app/components/IntroBgServer.tsx
+++ app/components/IntroBgServer.tsx
@@ -2,16 +2,36 @@
 
 import styles from '@/styles/IntroBg.module.css'
 import Link from 'next/link'
-import { getCopyRight } from '@/app/utils/GetBgcopyright'
+
 //also add another function to allow user auto pick a random wallpaper from 0-7
 const Bg = async () => {
   // const url = await Promise.all([BGData])
 
   // will quit using this server rendering as the vercel keeps giving weird bugs, suspect this api endpoint is blocked by the cloudware and whenever it triggers it will revalidate the ip
+  const fetchData = async () => {
+    const requestOptions: RequestInit = {
+      method: 'GET',
+      redirect: 'follow',
+      cache: 'no-store',
+    }
+    //'https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=en-CA'
+    //
+    const url =
+      'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
+    const response = await fetch(url, requestOptions)
 
-  const BGData: any = await getCopyRight()
-  const ImageLink = BGData.url
-  const copyright = BGData.copyright
+    console.log(response)
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const data = await response.json()
+    console.log(data)
+    return data
+  }
+
+  const BGData: any = await fetchData()
+  const ImageLink = 'https://www.bing.com/' + BGData.images[0].url
+  const copyright = BGData.images[0].copyright
   return (
     <>
       <div
--- app/utils/GetBgcopyright.tsx
+++ app/utils/GetBgcopyright.tsx
@@ -1,19 +1,34 @@
-export async function getCopyRight() {
-  const requestOptions: RequestInit = {
-    method: 'GET',
-    redirect: 'follow',
-  }
-  try {
-    const response = await fetch(
-      'https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=en-CA',
-      requestOptions
+'use client'
+import styles from '@/styles/IntroBg.module.css'
+import { useEffect, useState } from 'react'
+
+const getCopyRight = () => {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    fetch(
+      'https://bing.biturl.top/?resolution=1920&format=json&index=0&mkt=en-CA'
     )
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`)
-    }
-    const result = await response.json()
-    return result
-  } catch (error) {
-    console.error(error)
-  }
+      .then((resp) => resp.json())
+      .then((apiData) => {
+        setData(apiData.copyright)
+      })
+  }, [])
+
+  return (
+    <>
+      <div className={styles.footer}>
+        <div>
+          <p className={styles.HeaderFooter}>{data}</p>
+        </div>
+        <div>
+          <p className={styles.Description}>
+            This is a daily generated background image fetched from
+            BingWallpaper
+          </p>
+        </div>
+      </div>
+    </>
+  )
 }
+export default getCopyRight
