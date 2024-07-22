e22ddb2c8bd3e9eb1df6c287d851b0d5f8c3557d
rruiqiu <qiur12@mcmaster.ca>
2024-07-21 22:28:19 -0400

    add suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import BG from '@/app/components/IntroBgServer'
+import { Suspense } from 'react'
 const Home = () => {
   return (
     <main>
-      <BG></BG>
+      <Suspense fallback={<p>Loading ...</p>}>
+        <BG />
+      </Suspense>
     </main>
   )
 }
